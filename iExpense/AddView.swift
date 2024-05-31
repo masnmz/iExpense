407dd19ae4ce00efdcc3cdd6a0d21e13ab0d67d0
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-05-31 18:27:03 +0100

    Logic

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Mehmet Alp Sönmez on 31/05/2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
