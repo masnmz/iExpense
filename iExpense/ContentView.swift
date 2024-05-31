31ba55b574b564fceb9a6a6389ae67015417ce1a
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-05-31 14:33:40 +0100

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Mehmet Alp Sönmez on 30/05/2024.
+//
+
+
+import SwiftUI
+
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mehmet Alp Sönmez on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
